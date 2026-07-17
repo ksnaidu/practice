5137e99c0a1c0610ed9a04582bc8e5792930513a
ksnaidu <ksnaidudba@gmail.com>
2026-07-17 23:43:56 +0530

    bootstrap.sh

--- roboshop-infra-dev/40-databases/bootstrap.sh
+++ roboshop-infra-dev/40-databases/bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 component=$1
 dnf install ansible -y
-ansible-pull -u https://github.com/ksnaidu/11AM.git//ansible-roboshop-roles  -e component=$1 -e env=$2 main.yaml
+#ansible-pull -U https://github.com/ksnaidu/practice.git//ansible-roboshop-roles  -e component=$1 -e env=$2 main.yaml
+
+ansible-pull \
+  -U https://github.com/ksnaidu/practice.git \
+  -e component=$1 \
+  -e env=$2 \
+  ansible-roboshop-roles/main.yaml
